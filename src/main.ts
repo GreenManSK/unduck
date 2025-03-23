b7ba00127458d935139e8ab95adae5c4ed780cf1
Lukáš Kurčík <lukas.kurcik@gmail.com>
2025-03-23 13:56:33 +0100

    update url to gh-page

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ function noSearchDefaultPageRender() {
           <input 
             type="text" 
             class="url-input"
-            value="https://unduck.link?q=%s"
+            value="https://greenmansk.github.io/unduck?q=%s"
             readonly 
           />
           <button class="copy-button">
