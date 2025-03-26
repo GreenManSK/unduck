3b0ef06b1cff48ca05fa62ab3d351db224488c06
Lukáš Kurčík <lukas.kurcik@gmail.com>
2025-03-26 20:43:58 +0100

    try to reduce load a bit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
     }),
+    viteSingleFile(),
   ],
   base: "/unduck/",
 });
